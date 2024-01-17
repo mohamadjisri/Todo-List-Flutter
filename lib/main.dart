b85adb5d3b10112d930e799ea47180b8fb72b57d
mohamadjisri <mohammadjisri@gmail.com>
2024-01-17 10:16:57 +0100

    1. Todolist commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:thesis_todo_list/app/data/services/storage/services.dart';
+import 'package:thesis_todo_list/app/modules/home/view.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+void main() async {
+  await GetStorage.init();
+  await Get.putAsync(() => StorageService().init());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: 'Todo List Thesis',
+      home: HomePage(),
+    );
+  }
+}
